69f0b98acc233bd4541ce7180d9de2813efcbe87
NanYangEvelyn <ny2187@nyu.edu>
2023-09-23 18:45:01 -0400

    9/23/2023

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 let bigDiam = 50;
 let bigxPos = 0;
 let bigyPos = 0;
-let L = 100;
+let L = 200;
+
 
 let smallDiam = 20;
 let smallxPos = 0;
@@ -15,7 +16,7 @@ function setup() {
 function draw() {
     background(32,178,170);
     
-    //Big circle
+    //Big circle 1
     noStroke();
     fill(127,255,170);
     for(let bigxPos = 0; bigxPos <= windowWidth; bigxPos += L){
@@ -24,13 +25,32 @@ function draw() {
         }
     }
 
-   //Small circlr
+    //Big circle 2
+    noStroke();
+    fill(127,255,170);
+    for(let bigxPos = L/2; bigxPos <= windowWidth; bigxPos += L){
+        for(let bigyPos = L/2; bigyPos <= windowHeight; bigyPos += L){
+            circle(bigxPos,bigyPos,bigDiam);
+        }
+    }
+
+
+//    Small circlr 1
    noStroke();
    fill(0,191,255);
    for(let smallxPos = L/2; smallxPos <= windowWidth; smallxPos += L){
-    for(let smallyPos = L/2; smallyPos <= windowHeight; smallyPos += L){
+    for(let smallyPos = 0; smallyPos <= windowHeight; smallyPos += L){
         circle(smallxPos,smallyPos,smallDiam);
     }
+}
+
+//    Small circlr 2
+noStroke();
+fill(0,191,255);
+for(let smallxPos = 0; smallxPos <= windowWidth; smallxPos += L){
+ for(let smallyPos = L/2; smallyPos <= windowHeight; smallyPos += L){
+     circle(smallxPos,smallyPos,smallDiam);
+ }
 }
 
 
